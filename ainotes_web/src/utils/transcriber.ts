8f0abb01e41ff41e30eacc0d1b6e4b0d5c1d3a8b
EffDuBois <arnabkar2003@protonmail.com>
2024-08-23 22:36:32 +0530

    Frontend: WIP removed some bugs, have to memoise the connection object

--- ainotes_web/src/utils/transcriber.ts
+++ ainotes_web/src/utils/transcriber.ts
@@ -1,7 +1,58 @@
 "use client";
 import { createClient, ListenLiveClient } from "@deepgram/sdk";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 const deepgramKey = process.env.DEEPGRAM_API_KEY;
+
+function useTranscriber(placeholder: string) {
+  const [transcript, setTranscript] = useState(placeholder);
+  const [recording, setRecording] = useState(false);
+  const deepgram = createClient(deepgramKey);
+  const [mic, setMic] = useState<MediaRecorder>();
+  const [socket, setSocket] = useState<ListenLiveClient>(
+    deepgram.listen.live({
+      model: "nova",
+      smart_format: true,
+    })
+  );
+  socket.on("open", async () => {
+    console.log("client: connected to deepgram socket");
+    socket.on("Results", (data) => {
+      console.log(data);
+      const transcript = data.channel.alternatives[0].transcript;
+      if (transcript !== "") setTranscript((old) => old + transcript);
+    });
+    socket.on("error", (e) => console.error(e));
+    socket.on("warning", (e) => console.warn(e));
+    socket.on("Metadata", (e) => console.log(e));
+    socket.on("close", (e) => console.log(e));
+  });
+
+  const toggleTranscription = async () => {
+    if (!recording) {
+      console.log("Start Recording");
+
+      let newMic = await getMic();
+      setMic(newMic);
+      if (newMic && socket) {
+        startMic(newMic, socket);
+        setRecording(true);
+      } else {
+        console.log("mic not initialised");
+      }
+    } else {
+      if (mic) {
+        console.log("Stop Recording");
+        setRecording(false);
+        mic.stop();
+      } else {
+        console.error("Microphone not initialized");
+      }
+    }
+  };
+  return { toggleTranscription, transcript };
+}
+
+export default useTranscriber;
 
 const getMic = async () => {
   const userMedia = await navigator.mediaDevices.getUserMedia({
@@ -27,48 +78,3 @@ const startMic = async (mic: MediaRecorder, socket: ListenLiveClient) => {
     socket.send(data);
   };
 };
-
-function useTranscriber(placeholder: string) {
-  const [transcript, setTranscript] = useState(placeholder);
-  const [recording, setRecording] = useState(false);
-  const [mic, setMic] = useState<MediaRecorder>();
-
-  const deepgram = createClient(deepgramKey);
-  const socket = deepgram.listen.live({
-    model: "nova",
-    smart_format: true,
-  });
-  socket.on("open", async () => {
-    console.log("client: connected to deepgram socket");
-    socket.on("Results", (data) => {
-      console.log(data);
-      const transcript = data.channel.alternatives[0].transcript;
-      if (transcript !== "") setTranscript((old) => old + transcript);
-    });
-    socket.on("error", (e) => console.error(e));
-    socket.on("warning", (e) => console.warn(e));
-    socket.on("Metadata", (e) => console.log(e));
-    socket.on("close", (e) => console.log(e));
-  });
-
-  const toggleTranscription = async () => {
-    if (!recording) {
-      console.log("Start Recording");
-      setRecording(true);
-      const newMic = await getMic();
-      setMic(newMic);
-      startMic(newMic, socket);
-    } else {
-      if (mic) {
-        console.log("Stop Recording");
-        setRecording(false);
-        mic.stop();
-      } else {
-        console.error("Microphone not initialized");
-      }
-    }
-  };
-  return { toggleTranscription, transcript };
-}
-
-export default useTranscriber;
