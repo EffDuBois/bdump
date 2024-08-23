d7fc86e7b426b717061246e3773c6a3fd29aff37
EffDuBois <arnabkar2003@protonmail.com>
2024-08-23 13:07:22 +0530

    Made some design Changes

---
+++ ainotes_web/src/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { DM_Serif_Display, Indie_Flower, Lora } from "next/font/google";
+
+export const maintext = Lora({ subsets: ["latin"], weight: ["400"] });
+export const subtext = Indie_Flower({ subsets: ["latin"], weight: ["400"] });
+export const title = DM_Serif_Display({ subsets: ["latin"], weight: ["400"] });
