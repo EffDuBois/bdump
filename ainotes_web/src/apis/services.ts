f74bd6a2e77ecd4a233f37ff6c8f705e42775843
EffDuBois <arnabkar2003@protonmail.com>
2024-09-24 18:38:44 +0530

    Fixed backend url .env

--- ainotes_web/src/apis/services.ts
+++ ainotes_web/src/apis/services.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAPI = axios.create({
-  baseURL: process.env.BACKEND_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
 });
 
 export default baseAPI;
