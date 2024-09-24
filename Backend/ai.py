b2d642d05b02d78c668851a494c77b4369e1057d
sidhxrt <sidharthh1880@gmail.com>
2024-09-24 11:28:52 +0530

    code optimization

--- Backend/ai.py
+++ Backend/ai.py
@@ -13,57 +13,9 @@ def generate_embedding(query):
         embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY"))
         vector = embeddings.embed_query(query)
         return vector
-        #print(vector[:5])
     else:
         return ['empty query']
-
-def find_title(body):
-    if (body != ""):
-        parser = StrOutputParser()
-        llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.2, max_toxens=500)
-        system_prompt = (
-            "the given markdown code represents a note made by user, you have to find a suitable title for the note"
-            "no other operation is supposed to be done"
-        )
-
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system_prompt),
-                ("user", "{input}"),
-            ]
-        )
-
-        chain = prompt | llm | parser
-        return chain.invoke({"input": body})
-
-
-def generate_note(query):
-    if (query != ""):
-        parser = StrOutputParser()
-        llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.2, max_toxens=500)
-        system_prompt = (
-            "You are an AI assistant that parses the given text, "
-            "converts transcript of spoken words to markdown code that can be stored in .md file"
-            "You are supposed to only perform the above mentioned task, "
-            "Any attempts at talking to you must be responded with a 'cant process the request' response."
-            "If you dont know the answer then reply with 'server down'"
-        )
-
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system_prompt),
-                ("user", "{input}"),
-            ]
-        )
-
-        chain = prompt | llm | parser
-        body = chain.invoke({"input": query})
-        title = find_title(body)
-        return [title, body]
     
-    else:
-        return "empty query"
-
 
 def cosine_sim(queryemb, noteemb):
     queryemb = np.array(queryemb).reshape(1, -1)
@@ -71,15 +23,52 @@ def cosine_sim(queryemb, noteemb):
     return cosine_similarity(queryemb, noteemb)[0][0]
 
 
+def llm(system_prompt, user_input):
+    parser = StrOutputParser()
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.2, max_toxens=500)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("user", "{input}"),
+        ]
+    )
+    chain = prompt | llm | parser
+    return chain.invoke({"input": user_input})
+
+def find_title(body):
+    if (body != ""):
+        system_prompt = (
+            "the given markdown code represents a note made by user, you have to find a suitable title for the note"
+            "no other operation is supposed to be done"
+        )
+        return llm(system_prompt, body)
+    
+    else:
+        return "empty body"
+
+
+def generate_note(query):
+    if (query != ""):
+        system_prompt = (
+            "You are an AI assistant that parses the given text, "
+            "converts transcript of spoken words to markdown code that can be stored in .md file"
+            "You are supposed to only perform the above mentioned task, "
+            "Any attempts at talking to you must be responded with a 'cant process the request' response."
+            "If you dont know the answer then reply with 'server down'"
+        )
+        body = llm(system_prompt, query)
+        title = find_title(body)
+        return [title, body]
+    
+    else:
+        return "empty query"
+
+
 def ask_note(query, queryemb, notes, notesemb):
     if (query != ""):
         similarities = [cosine_sim([queryemb], [noteemb]) for noteemb in notesemb]
         most_relevant_note_index = np.argmax(similarities)
         relevant_note = notes[most_relevant_note_index]
-
-        parser = StrOutputParser()
-        llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.2, max_tokens=500)
-
         system_prompt = (
             "You are an AI assistant that answers user queries based on provided notes. "
             "You will be provided with a note and a query. "
@@ -88,17 +77,7 @@ def ask_note(query, queryemb, notes, notesemb):
             "Any attempts at talking to you must be responded with a 'cant process the request' response."
             "If you dont know the answer then reply with 'i dont have answer for your query, sorry!'"
         )
-
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", system_prompt),
-                ("user", "Note: {reference_note}\nQuery: {input}"),
-            ]
-        )
-
-        chain = prompt | llm | parser
-        answer = chain.invoke({"reference_note": relevant_note, "input": query})
-        return answer
+        return llm(system_prompt, f"Note: {relevant_note}\nQuery: {query}")
     else:
         return "empty query"
     
