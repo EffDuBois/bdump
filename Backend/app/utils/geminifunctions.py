7dc43fe566e980d10e68756a857ccfa08278b0a6
Niktenj14 <tenjarlanikita14@gmail.com>
2024-12-26 01:15:40 +0530

    edit implemented

--- Backend/app/utils/geminifunctions.py
+++ Backend/app/utils/geminifunctions.py
@@ -6,7 +6,7 @@ from app.logger import setup_logger
 from app.utils.aimath import cosinesim
 from app.utils.datavalidation import askOutput, createOutput
 from app.utils.grokfunctions import grok_ask_note, grok_create_note
-from app.utils.prompts import ASK_NOTES_PROMPT, CREATE_NOTES_PROMPT
+from app.utils.prompts import ASK_NOTES_PROMPT, CREATE_NOTES_PROMPT, EDIT_NOTE_PROMPT
 from fastapi import HTTPException
 import google.generativeai as genai
 
@@ -88,5 +88,40 @@ def ask_note(query, queryemb, notes, notesemb):
                 logger.error("Maximum retries reached for Gemini, shifting to Grok.")
                 grok_ask_response = grok_ask_note(query, queryemb, notes, notesemb) 
                 return grok_ask_response 
+            time.sleep(backoff_time)
+            backoff_time = backoff_time * 2
+
+def edit_note(editinput):
+    MAX_RETRY = 3
+    attempt = 0
+    backoff_time = 1  # initial backoff time
+    query= editinput.title
+    note=editinput.note
+
+
+    while attempt < MAX_RETRY:
+        try:
+            if (note != ""):
+                messages = f"system: {EDIT_NOTE_PROMPT}\nuser: (Note: {note}, Query:{query})"
+                model = genai.GenerativeModel("gemini-1.5-pro-latest")
+                result = model.generate_content(
+                    messages,
+                    generation_config=genai.GenerationConfig(
+                        temperature=0.2,      
+                        max_output_tokens=300,
+                        response_mime_type="application/json", response_schema=list[createOutput]
+                    ),
+                )
+                raw_output = result.candidates[0].content.parts[0].text  
+                output = json.loads(raw_output) 
+                return output[0]
+            else:
+                return "empty query"
+        except Exception as e:
+            attempt += 1
+            logger.error(f"Attempt {attempt} failed: {e}")
+            
+            if attempt == MAX_RETRY:
+                raise HTTPException(status_code=503,detail="llm api is exhausted")
             time.sleep(backoff_time)
             backoff_time = backoff_time * 2
