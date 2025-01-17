b4fce94f66790991fae959b1afa8444f831dcdeb
sidhxrt <sidharthh1880@gmail.com>
2025-01-17 23:48:17 +0530

    mongodb setup for logging

--- Backend/app/routers/v0/root.py
+++ Backend/app/routers/v0/root.py
@@ -2,16 +2,33 @@ from fastapi import APIRouter
 from app.utils.geminifunctions import generate_embedding, create_note, ask_note, edit_note
 from app.utils.datavalidation import askPrompt, createPrompt, editPrompt
 from app.logger import setup_logger
- 
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 router = APIRouter()
 
 logger = setup_logger()
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+db = client["BrainDump_Mixer"]
+create_collection = db["create"]
+ask_collection = db["ask"]
+edit_collection = db["edit"]
 
 @router.post("/notes/create")
 async def create_notes(prompt: createPrompt):       
     emb = generate_embedding(prompt.query)             # returns vector embedding for the prompt
     ans = create_note(prompt.query)                  # returns markdown text for the prompt
     logger.info(f"note created with title: {ans}" )
+    data = {
+        'input-text': prompt.query,
+        'processed-text': ans
+    }
+    create_collection.insert_one({'query': data})
     output = {**ans, **emb}
     return output
 
@@ -22,6 +39,11 @@ async def ask_notes(prompt: askPrompt):
     notesemb = [i.embedding for i in prompt.data]
     ans = ask_note(prompt.query, emb["embedding"], notes_text, notesemb)
     logger.info(f"answer for query: {ans}")
+    data = {
+        'input-text': prompt.query,
+        'processed-text': ans
+    }
+    ask_collection.insert_one({'query': data})
     return ans
 
 @router.post("/notes/edit")
@@ -31,5 +53,10 @@ async def edit_notes(prompt: editPrompt):
     ans = edit_note(note, query)  
     emb = generate_embedding(ans['note'])                   
     logger.info(f"note edited with title: {ans}" )
+    data = {
+        'input-text': prompt.query,
+        'processed-text': ans
+    }
+    edit_collection.insert_one({'query': data})
     output = {**ans, **emb}
     return output
